797b319dc9c6daadf9d3f6e8a08cb352c2325895
jorell28 <140922322+jorell28@users.noreply.github.com>
2023-08-02 11:54:35 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,60 @@
+
+scene.onOverlapTile(SpriteKind.Player, img`
+            5 4 4 5 5 4 4 4 4 2 2 2 4 4 4 4
+            4 4 4 4 4 5 5 4 2 2 2 2 4 4 4 5
+            4 2 2 2 4 4 5 4 2 2 4 4 5 5 5 5
+            2 2 4 2 4 4 5 4 2 2 4 5 5 5 5 4
+            2 2 2 2 4 4 5 4 2 2 4 4 5 5 4 4
+            4 2 2 2 4 5 5 4 4 4 4 4 4 4 4 2
+            2 2 2 4 4 5 5 5 4 4 2 2 2 2 2 2
+            4 2 2 4 5 5 5 5 4 2 2 4 2 2 2 4
+            5 4 4 4 4 4 4 5 5 4 2 2 2 4 4 4
+            4 4 4 2 2 2 4 4 5 5 4 4 4 4 5 5
+            4 2 2 2 2 2 2 2 4 5 5 5 5 5 5 5
+            5 4 4 2 4 2 2 4 4 5 5 5 4 4 4 5
+            5 5 4 2 2 2 4 4 4 5 5 4 2 2 2 4
+            4 5 4 4 4 4 5 5 5 5 4 2 4 2 2 4
+            4 5 5 5 5 5 5 4 4 4 2 4 2 4 2 4
+            4 5 5 5 4 4 4 4 2 2 2 2 4 2 4 4
+        `, function (sprite, location) {
+    game.setGameOverMessage(true, "GAME OVER!")
+})
+let mySprite = sprites.create(img`
+    . . . . . . . . . . 
+    . f f f f f f f f f 
+    4 4 4 4 4 4 4 4 4 4 
+    4 4 f f f f f f f f 
+    . f f b f f f b f f 
+    . f f b f f f b f f 
+    . f f b f f f b f f 
+    . f f f f f f f f f 
+    . f f f f f f f f f 
+    . f f f f f f f f f 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100)
+tiles.setCurrentTilemap(tilemap`level4`)
+scene.cameraFollowSprite(mySprite)
+let mySprite2 = sprites.create(img`
+    d d d d d d d c c d d d d d d d
+    d d d d c c c 6 5 c 6 6 d d d d
+    d d d d c 6 c 5 5 c 7 6 d d d d
+    d d d 6 c c 7 5 5 7 c 6 6 d d d
+    d d c c 7 7 7 7 7 5 7 7 c 6 d d
+    d 6 6 6 c 6 7 7 7 7 6 c c 6 6 d
+    c 7 7 7 6 c 7 c 6 7 6 7 7 7 7 6
+    c c c 6 6 6 c 6 6 6 6 7 7 6 6 6
+    d c c 7 6 6 6 6 6 7 7 7 7 c 6 d
+    d c 7 7 6 6 7 6 6 7 7 6 7 7 c d
+    d c c c c 7 7 6 f 7 7 c c c c d
+    d d d d c 7 c f f c 7 c d d d d
+    d d d d d 6 f e e e c d d d d d
+    d d d d d e e e e e e d d d d d
+    d d d d e e d e e d e e d d d d
+    d d d d d d d e e d d d d d d d
+`, SpriteKind.Food)
+mySprite2.setPosition(245, 245)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function(sprite: Sprite, otherSprite: Sprite) {
+    game.gameOver(true)
+})
+game.showLongText("Use arrow keys to move!", DialogLayout.Bottom)
+game.showLongText("   Help Cole find the Traveller's Tree!", DialogLayout.Bottom)
---
+++ tilemap.g.ts
@@ -0,0 +1,81 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level":
+            case "level1":return tiles.createTilemap(hex`1000100005020202020a090202020202020209060107090a0a0a09070a0a0a0a0a070908010709090909090707070707070709030107070a0a0a0907070909090707090a0107070a0a0a0907070a090a070a090a010a070707070707070a090a0709090a010a07070707070a0a0a090a070a090a010a090909090909090909090707090a010a070707070709090a0a0a0707090a010a0709070a0a07090a0a07070a090a010a0709070a0a07090707070a09090a0107070909070709090707090909090a0107090909090909090707090a0a07030a0a070707090a090a0707090a090709090a0a07070a0a090a0707090a09070a09090a0404040404040404040a090a0a`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath4,sprites.castle.tilePath2,sprites.castle.tilePath6,sprites.castle.tilePath8,sprites.castle.tilePath1,sprites.castle.tilePath3,sprites.castle.tilePath5,sprites.builtin.forestTiles0,sprites.dungeon.hazardLava1,myTiles.tile1], TileScale.Sixteen);
+            case "level0":
+            case "level2":return tiles.createTilemap(hex`1000100005020202020a090202020202020209060107090a0a0a09070a0a0a0a0a070908010709090909090707070707070709030107070a0a0a0907070909090707090a0107070a0a0a0907070a090a070a090a010a070707070707070a090a0709090a010a07070707070a0a0a090a070a090a010a090909090909090909090707090a010a070707070709090a0a0a0707090a010a0709070a0a07090a0a07070a090a010a0709070a0a07090707070a09090a0107070909070709090707090909090a0107090909090909090707090a0a07030a0a070707090a090a0707090a090709090a0a07070a0a090a0707090a09070a09090a0404040404040404040a090a0a`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath4,sprites.castle.tilePath2,sprites.castle.tilePath6,sprites.castle.tilePath8,sprites.castle.tilePath1,sprites.castle.tilePath3,sprites.castle.tilePath5,sprites.builtin.forestTiles0,sprites.dungeon.hazardLava1,myTiles.tile1], TileScale.Sixteen);
+            case "level4":
+            case "level3":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
+. . . 2 . . . 2 . . . . 2 . . . 
+. . . 2 . . . 2 . . . . 2 . 2 2 
+. 2 2 2 . . . 2 2 2 . . 2 . . . 
+. . . 2 . . . 2 . . . 2 2 2 2 . 
+. . . . . . . . . . . 2 . . . . 
+2 2 2 2 2 2 . . . . . 2 . . . . 
+. . 2 . . . . . 2 . . . . 2 2 2 
+. . 2 . . . . . 2 . . . . . . 2 
+. . 2 . . 2 2 2 2 2 2 2 2 . . 2 
+. . 2 . . . . . 2 . . 2 . . . . 
+. . 2 . . . . . 2 . . 2 . . . . 
+. . . . . . . . 2 . . . . 2 . . 
+2 2 2 2 2 2 2 . 2 2 . . . 2 2 2 
+. . . 2 . 2 . . . 2 2 . . 2 . . 
+. 2 . 2 . . . . . . 2 2 2 2 . . 
+. 2 . . . 2 . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath5], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
